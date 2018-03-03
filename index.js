e06d5b6a89b272691b6f1c99df195b1e09195955
DESKTOP-SG8T284\sharun <sharunkk1997@gmail.com>
2018-03-03 13:48:01 +0530

    b
    b

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ var mainText = document.getElementById('mainText');
 var btnc = document.getElementById('btnc');
 
 function cliCked() {
-
+  var selectedVal = "";
+  var selected = $("input[type='radio'][name='eve']:checked");
+  if (selected.length > 0) {
+      selectedVal = selected.val();
+  }
 var firebaseRef = firebase.database().ref();
 var message = mainText.value;
-firebaseRef.child("usman").set(message);
+firebaseRef.child("sssffd").set(message);
+firebaseRef.child("sdsffsd").set(selectedVal);
 }
