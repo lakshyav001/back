b3538a958e9b68661c795a034d13fc98e9389ec6
DESKTOP-SG8T284\sharun <sharunkk1997@gmail.com>
2018-03-03 12:07:29 +0530

    added
    firebase worked

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var main_page = getElementById('main_page');
-var datawe = getElementById('datawe');
 
+var mainText = document.getElementById('mainText');
 
-function () {
+var btnc = document.getElementById('btnc');
 
+function cliCked() {
+var firebaseRef = firebase.database().ref();
+firebaseRef.child("usman").set(" are cool");
 }
